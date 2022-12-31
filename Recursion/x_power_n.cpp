f729dfef752264cc6a15c05517f96fee56fd8a65
Aditya Kaushik <adityakaushik373@gmail.com>
2023-01-01 00:07:40 +0530

    new year commit :)

---
+++ Recursion/x_power_n.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int xPowern(int x, int n)
+{
+    if (n == 0)
+        return 1;
+    // if(n==1) return n;
+
+    return x * xPowern(x, n - 1);
+}
+
+int main()
+{
+    int x, n;
+    cin >> x >> n;
+    cout << xPowern(x, n)<<endl;
+    
+    
+
+}
